64dc9621332e4193d3e174ecb6c7cce29065963a
Jean Louis <jeanlouis@grutto.com>
2022-11-04 17:10:01 +0100

    board stuff

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 from flask import Flask
+from godoku import GoDoku
 
 app = Flask(__name__)
 
 @app.route("/api")
 def hello_world():
     return "jloku api!"
+
+@app.route("/api/generate_board")
+def generate_board():
+    g = GoDoku()
+    return g.generate_puzzle()
+
+@app.route("/api/get_daily_board")
+def get_daily_board():
+    g = GoDoku()
+    return g.get_daily_board()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
