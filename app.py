d5eefeedee021a8d21248a94e08c871dd3c04c8c
Jean Louis <jeanlouis@grutto.com>
2022-11-04 22:52:19 +0100

    APPLICATION_ROOT

--- app.py
+++ app.py
@@ -2,17 +2,19 @@ from flask import Flask
 from godoku import GoDoku
 
 app = Flask(__name__)
+app.config['APPLICATION_ROOT'] = '/api'
 
-@app.route("/api")
+
+@app.route("/")
 def hello_world():
     return "jloku api!"
 
-@app.route("/api/generate_board")
+@app.route("/generate_board")
 def generate_board():
     g = GoDoku()
     return g.generate_puzzle()
 
-@app.route("/api/get_daily_board")
+@app.route("/get_daily_board")
 def get_daily_board():
     g = GoDoku()
     return g.get_daily_board()
