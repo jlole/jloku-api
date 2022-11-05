7d1f0e13b64f63f9340b127f72c97abb64f41702
Jean Louis <jeanlouis@grutto.com>
2022-11-05 13:13:33 +0100

    uncomment code

--- database.py
+++ database.py
@@ -1,14 +1,13 @@
 import os
 from pymongo import MongoClient
 class Database():
-  # MONGO_USERNAME = os.getenv('MONGO_USERNAME')
-  # MONGO_PASSWORD = os.getenv('MONGO_PASSWORD')
-  # client = MongoClient("mongodb+srv://" + MONGO_USERNAME + ":" + MONGO_PASSWORD + "@cluster0.dgbj2.mongodb.net/?retryWrites=true&w=majority")
-  # db = client['jloku']
+  MONGO_USERNAME = os.getenv('MONGO_USERNAME')
+  MONGO_PASSWORD = os.getenv('MONGO_PASSWORD')
+  client = MongoClient("mongodb+srv://" + MONGO_USERNAME + ":" + MONGO_PASSWORD + "@cluster0.dgbj2.mongodb.net/?retryWrites=true&w=majority")
+  db = client['jloku']
 
   def get_daily(self):
-    return 'o'
-    # collection = self.db["daily_godokus"]
-    # item_details = collection.find()
-    # for item in item_details:
-      # return item['puzzle']
+    collection = self.db["daily_godokus"]
+    item_details = collection.find()
+    for item in item_details:
+      return item['puzzle']
