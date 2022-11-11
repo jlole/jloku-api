cb692679380303f9f0e79756b1cd586644d8de4d
Jean Louis <jeanlouis@grutto.com>
2022-11-11 01:00:40 +0100

    update endpoint

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask import request
-from sudoku import Board
-from database import Database
+from jloku.sudoku import Board
+from jloku.database import Database
 
 app = Flask(__name__)
 
@@ -11,12 +11,12 @@ def api_home():
     return "Welcome to the jloku api!"
 
 
-@app.route("/generate_board")
+@app.route("/get-new-puzzle")
 def generate_board():
     return Board().generate_puzzle(request.args.get('difficulty'))
 
 
-@app.route("/get_daily_board")
+@app.route("/get-daily-puzzle")
 def get_daily_board():
     return Database().get_daily()
 
